7c979c52ed63515a703e9fce8e5022803512e89f
twopiii <code.besaytejomarkristoffer@gmail.com>
2025-08-09 02:36:26 +0800

    feat: added functionality to country card function

--- mini-country-viewer/src/components/CountryCard.jsx
+++ mini-country-viewer/src/components/CountryCard.jsx
@@ -9,11 +9,23 @@ const CountryCard = ({ country }) => {
     setIsExpanded(!isExpanded);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" || e.key === "") {
+      e.preventDefault();
+      toggleCard();
+    }
+  };
+
   return (
     <div className="card-container basis-full md:basis-2xs lg:basis-2xs">
       <div
         className="p-4 cursor-pointer bg-white rounded-md shadow-lg hover:bg-gray-100
                focus:outline-none focus:ring-2 focus:ring-blue-500"
+        tabIndex="0"
+        role="button"
+        onClick={toggleCard}
+        onKeyDown={handleKeyDown}
+        aria-expanded={isExpanded}
       >
         <div className="flex items-center">
           <img
@@ -30,6 +42,19 @@ const CountryCard = ({ country }) => {
             <h3 className="text-sm font-bold">{country.region}</h3>
           </div>
         </div>
+
+        {isExpanded && (
+          <div className="other-details mt-4 pt-4 border-t border-gray-200 text-sm">
+            <p>
+              <span>Capital: </span>
+              {country.capital}
+            </p>
+            <p>
+              <span>Population: </span>
+              {country.population.toLocaleString()}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
